217da2da9c4958fd70bf9e50f91880aa366cfbc2
Pascal Senn <pascal.senn@gia.ch>
2019-10-22 17:01:23 +0200

    feat(Sort): add sorting algorithms

--- ch.fhnw.algd1.abstracttypes/src/main/java/ch/fhnw/algd1/maxsubsequence/MaxSubProblem.java
+++ ch.fhnw.algd1.abstracttypes/src/main/java/ch/fhnw/algd1/maxsubsequence/MaxSubProblem.java
@@ -2,9 +2,74 @@ package ch.fhnw.algd1.maxsubsequence;
 
 public class MaxSubProblem {
 
-	public int maxSub(int[] data){
-		//TODO (A1) Implement your max sub-sequence algorithm here
-		return 0;
-	}
-		
+    public int maxSub(int[] data) {
+        var lastLow = 0;
+        var lastHigh = 0;
+        var bestSequence = 0;
+        var last = 0;
+        for (var i = 0; i < data.length; i++) {
+            var current = data[i];
+            var next = last + current;
+            if (last + current < lastLow) {
+                lastLow = next;
+                lastHigh = next;
+            } else if (last + current > lastHigh) {
+                lastHigh = next;
+                if (bestSequence < lastHigh - lastLow) {
+                    bestSequence = lastHigh - lastLow;
+                }
+            }
+            last = next;
+        }
+
+        return bestSequence;
+    }
+
+    public int maxSubv3(int[] data) {
+        var max = 0;
+        for (var start = 0; start < data.length; start++) {
+            var temp = 0;
+            if (start == 0 || data[start - 1] < 0) {
+                for (var end = start; end < data.length; end++) {
+                    temp += data[end];
+                    if (temp > max) {
+                        max = temp;
+                    }
+                }
+            }
+
+        }
+        return max;
+    }
+
+    public int maxSubv2(int[] data) {
+        var max = 0;
+        for (var start = 0; start < data.length; start++) {
+            var temp = 0;
+            for (var end = start; end < data.length; end++) {
+                temp += data[end];
+                if (temp > max) {
+                    max = temp;
+                }
+            }
+        }
+        return max;
+    }
+
+    public int maxSubv1(int[] data) {
+        var max = 0;
+        for (var start = 0; start < data.length; start++) {
+            for (var end = start; end < data.length; end++) {
+                int temp = data[start];
+                for (var index = start + 1; index <= end; index++) {
+                    temp += data[index];
+                }
+                if (temp > max) {
+                    max = temp;
+                }
+            }
+        }
+        return max;
+    }
+
 }
--- ch.fhnw.algd1.abstracttypes/src/main/java/ch/fhnw/algd1/simplearraylist/SimpleArrayList.java
+++ ch.fhnw.algd1.abstracttypes/src/main/java/ch/fhnw/algd1/simplearraylist/SimpleArrayList.java
@@ -4,44 +4,70 @@ import java.util.AbstractList;
 import java.util.List;
 
 public class SimpleArrayList<E> extends AbstractList<E> implements List<E> {
-	private static final int MIN_ARRAY_LEN = 16;
-	private Object[] arr = new Object[MIN_ARRAY_LEN];
-	private int size = 0;
+    private static final int MIN_ARRAY_LEN = 16;
+    private Object[] arr = new Object[MIN_ARRAY_LEN];
+    private int size = 0;
 
-	@Override
-	public int size() {
-		return size;
-	}
+    @Override
+    public int size() {
+        return size;
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public E get(int index) {
-		// TODO (A2) check preconditions
-		return (E)arr[index];
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return (E) arr[index];
+    }
 
-	@Override
-	public E set(int index, E element) {
-		E old = get(index); // includes index checking
-		arr[index] = element;
-		return old;
-	}
+    @Override
+    public E set(int index, E element) {
+        E old = get(index); // includes index checking
+        arr[index] = element;
+        return old;
+    }
 
-	@Override
-	public void add(int index, E element) {
-		// TODO (A2) implement adding an element in correct position
-	}
+    @Override
+    public void add(int index, E element) {
+        if (index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (arr.length - 1 == size) {
+            this.resizeArray(size + MIN_ARRAY_LEN);
+        }
+        var counter = size;
+        while (counter > index) {
+            arr[counter] = arr[counter - 1];
+            counter--;
+        }
+        arr[index] = element;
+        size++;
+    }
 
-	@Override
-	public E remove(int index) {
-		E old = get(index); // includes index checking
-		while (index + 1 < size) {
-			arr[index] = arr[index + 1];
-			index++;
-		}
-		arr[size] = null;
-		size--;
-		// TODO (A2) optional: reduce list size, if possible / appropriate
-		return old;
-	}
+    @Override
+    public E remove(int index) {
+        E old = get(index); // includes index checking
+        while (index + 1 < size) {
+            arr[index] = arr[index + 1];
+            index++;
+        }
+        size--;
+        arr[size] = null;
+        if (arr.length - MIN_ARRAY_LEN > size) {
+            resizeArray(arr.length - MIN_ARRAY_LEN);
+        }
+        return old;
+    }
+
+    private void resizeArray(int newSize) {
+        var tempArray = new Object[newSize];
+        var index = 0;
+        while (index < arr.length && index < tempArray.length) {
+            tempArray[index] = arr[index];
+            index++;
+        }
+        arr = tempArray;
+    }
 }
